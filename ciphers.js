766aa73de4ea68780988952d242a59e604182e8c
Anna Spencer <annapeel@hotmail.com>
2020-06-05 19:19:23 -0700

    added cipher functions

--- ciphers.js
+++ ciphers.js
@@ -1,61 +1,63 @@
 //atbash cipher
-const alphabet = `abcdefghijklmnopqrstuvwxyz`.split('')
-const cipherer = [...alphabet].reverse()
-const encoder = alphabet.reduce((o, c, i) => {
-  o[c] = cipherer[i]
-  return o
-}, {})
-const decoder = cipherer.reduce((o, c, i) => {
-  o[c] = alphabet[i]
-  return o
-}, {})
+atbash("ZYX");
+function atbash(message) {
+var alphabet = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz ";
+var tebahpla = "ZzYyXxWwVvUuTtSsRrQqPpOoNnMmLlKkJjIiHhGgFfEeDdCcBbAa ";
+var decoded_string = "";
 
-const encode = str => {
-  let arr = str
-    .toLowerCase()
-    .split('')
-    .filter(c => cipherer.includes(c))
-    .map(c => encoder[c])
-  
-  let i = -1
-  let final = []
-
-  arr.forEach(c => {
-    i++
-    final.push(c)
-    if (i === 4) {
-      final.push(' ')
-      i = -1
-    }
-  })
-
-  return final.join('')
+for (i = 0; i < message.length; i++) {
+    var coded_letter = message.charAt(i);
+    var letter_index = alphabet.indexOf(coded_letter);
+    var decoded_letter = tebahpla.charAt(letter_index);
+    decoded_string = decoded_string + decoded_letter;
+}
+return decoded_string;
 }
 
-const decode = str => {
-  let arr = str
-    .split('')
-    .map(c => decoder[c])
-  return arr.join('')
+document.forms["atbash"].onsubmit = function(){
+  var s = document.getElementById("atbash").value;
+  var f = atbash(s);
+  alert(f);
+  return false;
 }
-
-let msg = `The quick brown fox jumps over the lazy dog`
-let encoded = encode(msg)
-let decoded = decode(encoded)
 
 
 //ROT13 function
 
-function rot13(str) {
-    var input     = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
-    var output    = 'NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm';
-    var index     = x => input.indexOf(x);
-    var translate = x => index(x) > -1 ? output[index(x)] : x;
-    return str.split('').map(translate).join('');
-  }
-
+function rot13Fast(str) { 
+  return str.split('').map(x => rot13Fast.lookup[x] || x).join('')
+}
+rot13Fast.input  = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'.split('')
+rot13Fast.output = 'NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm'.split('')
+rot13Fast.lookup = rot13Fast.input.reduce((m,k,i) => Object.assign(m, {[k]: rot13Fast.output[i]}), {})
+  
+document.forms["rot13"].onsubmit = function(){
+    var s = document.getElementById("rot13").value;
+    var f = rot13Fast(s);
+    alert(f);
+    return false;
+}
   //caesarcipher
 
-  function caesarCipher(str, key) {
-    return str.toUpperCase().replace(/[A-Z]/g, c => String.fromCharCode((c.charCodeAt(0)-65 + key ) % 26 + 65));
+  function isUpperCase(str) {
+    return str === str.toUpperCase();
+}
+
+function caesarCipher(str, key = 3){
+  let decipher = '';
+  for(let i = 0; i < str.length; i++){
+    if(isUpperCase(str[i])){
+      decipher += String.fromCharCode((str.charCodeAt(i) + key - 65) % 26 + 65);
+    }else{
+      decipher += String.fromCharCode((str.charCodeAt(i) + key - 97) % 26 + 97);
+    }
   }
+  return decipher;
+}
+
+document.forms["caesarCipher"].onsubmit = function(){
+  var s = document.getElementById("caesarCipher").value;
+  var f = caesarCipher(s);
+  alert(f);
+  return false;
+}
